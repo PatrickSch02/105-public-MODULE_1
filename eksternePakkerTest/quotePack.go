619ef5f5c2c7f1451df35d5b3d35ab649f62440a
PatrickSch02 <PatrickSc@uia.no>
2022-01-26 19:05:26 +0100

    made stuff

---
+++ eksternePakkerTest/quotePack.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+
+"rsc.io/quote"
+"fmt"
+)
+
+func main(){
+
+fmt.Println(quote.Hello())
+fmt.Println(quote.Glass())
+fmt.Println(quote.Go())
+fmt.Println(quote.Opt())
+
+}
